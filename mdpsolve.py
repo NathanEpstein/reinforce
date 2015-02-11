1bd5c3850431ea6427b0459938f855ed15e127fd
NathanEpstein <ne2210@columbia.edu>
2015-02-11 09:35:47 -0500

    added convergence checking to solver

--- mdpsolve.py
+++ mdpsolve.py
@@ -1,12 +1,20 @@
+def checkConverge(new,old):
+  totalDif = 0
+  totalOld = 0
+  for i in range(0,len(old)):
+    totalDif += abs(new[i] - old[i])
+    totalOld += abs(old[i])
+  return (totalDif < 0.001*totalOld)
+
+
 #S(states) and A(actions) are implicitly integers implicitly defined by P
 #(P[state][action][state_] = p(state->state_|action)
 def policy(P,gamma,R):
   pol = [0]*len(P)
   V = [0] * len(P)
-  #update 7-9 later to be convergence instead of just many loops
-  count = 0
-  while (count < 1000):
-    count += 1
+  converged = False
+  while not (converged):
+    V_ = V[:] #track previous iteration for comparison
     #iterate over each state
     for s in range(0,len(P)):
       futureVal = -float('Inf')
@@ -21,13 +29,14 @@ def policy(P,gamma,R):
           futureVal = val
           pol[s] = a
       V[s] = R[s] + futureVal
+    converged = checkConverge(V, V_)
   return pol
 
 
 def main():
-  P = [[[0.25,0.25,0.5],[0,1,0],[0,0,1]],[[0.25,0.25,0.5],[0,0,1],[0,1,0]],[[0.25,0.25,0.5],[0,0,1],[0,1,0]]]
+  P = [[[0.25,0.25,0.5],[0,0,1],[0,1,0]],[[0.25,0.25,0.5],[0,1,0],[0,0,1]],[[0.25,0.25,0.5],[0,0,1],[0,1,0]]]
   gamma = 0.95
-  R = [1,10,1]
+  R = [5,-1,-1]
 
   x = policy(P,gamma,R)
   print(x)
