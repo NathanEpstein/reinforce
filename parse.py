082f966b17944ea4d0b9c3e521deb57e11d680f2
NathanEpstein <ne2210@columbia.edu>
2015-02-12 10:46:54 -0500

    initial version of parse.py complete

--- parse.py
+++ parse.py
@@ -3,24 +3,30 @@ from __future__ import division
 #create maps from states<->ints, actions<->ints
 #convert single reward to step rewards if applicable
 def parse(*args):
+  obs = args[0]
   # obs and R, 1 reward per observation
+  # convert obs to include rewards each step
   if len(args) > 1:
-    obs = args[0]
     R = args[1]
-  # only obs, 1 reward per step in each observation
-  else:
-    obs = args[0]
-    actMap = []
-    stateMap = []
     for o in range(0,len(obs)):
-      for t in range(0,len(obs[o])):
-        state = obs[o][t][0]
-        action = obs[o][t][1]
-        # reward = obs[o][t][2]
-        if (state not in stateMap):
-          stateMap.append(state)
-        obs[o][t][0] = stateMap.index(state)
-        if (action not in actMap):
-          actMap.append(action)
-        obs[o][t][1] = actMap.index(action)
-    return [stateMap,actMap]
+      totalReward = R[o]
+      stepReward = totalReward/len(obs[o])
+      for t in range (0,len(obs[o])):
+        obs[o][t].append(stepReward)
+
+  #create maps from actions and states to integers
+  actMap = []
+  stateMap = []
+  for o in range(0,len(obs)):
+    for t in range(0,len(obs[o])):
+      state = obs[o][t][0]
+      action = obs[o][t][1]
+      # reward = obs[o][t][2]
+      if (state not in stateMap):
+        stateMap.append(state)
+      obs[o][t][0] = stateMap.index(state)
+      if (action not in actMap):
+        actMap.append(action)
+      obs[o][t][1] = actMap.index(action)
+  return [stateMap,actMap,obs]
+
